3ef17f221287385b850cc89b8848685444cc318f
Chr1s28 <cbosshard28@gmail.com>
2026-03-20 22:53:39 +0100

    add nav to streamlit

--- deployment/app.py
+++ deployment/app.py
@@ -1,3 +1,13 @@
 import streamlit as st
 
-st.title("VDSS Streamlit App")
+def home_page():
+    st.title("VDSS Streamlit App")
+
+home = st.Page(home_page, title="Home", default=True)
+nav = st.navigation([home], position="hidden")
+
+with st.sidebar:
+    st.page_link(home, label="Home")
+    st.page_link("https://vdss.cboss.dev/", label="Documentation")
+
+nav.run()
